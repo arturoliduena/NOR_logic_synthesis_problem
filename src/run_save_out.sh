1487905dea4b9de8157dac3d50117a4c7da1ae93
Arturo Lidueña <arturo.liduena@gmail.com>
2024-04-07 11:56:30 +0200

    adding out folder (results)

--- src/run_save_out.sh
+++ src/run_save_out.sh
@@ -10,10 +10,23 @@ cleanup() {
 # Trap SIGINT signal (Ctrl+C)
 trap cleanup SIGINT
 
+
+# Initialize counters
+num=0
+error_num=0
+
+# Create output directory if it does not exist
+mkdir -p ../out
+
 for instance in ../instances/nlsp_*.inp; do
+    ((num++))
+    echo "Running instance $instance"
+    echo "Instance processing number $num"
     output_file="../out/$(basename "$instance" .inp).out"
     gtimeout 60s ./nlsp < "$instance" > "$output_file"
     if [ ! -s "$output_file" ]; then
+        ((error_num++))
+        echo "Error processing instance $instance (Error $error_num)"
         rm "$output_file"
     fi
     echo "................"
